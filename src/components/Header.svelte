48c96c93a8912933f0b3fbfce588c8f79c3d0f9c
glarexx <aagrwal452@gmail.com>
2023-01-07 22:28:51 +0530

    navbar and icon chnages

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <header>
-  <h1><br>{heading} <br><br></h1>
+  <h1>{heading}</h1>
   {#if hasHomeBtn}
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div  on:click={changePageHandler}>
@@ -20,18 +20,26 @@
 </header>
 
 <style>
+  header{
+    background-color: #0066FF;
+    margin-left: -7px;
+    margin-right: -6px;
+   
+  }
   h1{
     font-family: 'Inter', sans-serif;
     color:white;
     text-align: center;
-    background-color: #0066FF;
     margin-top: -10px;
     margin-bottom:10px;
-    margin-left: -10px;
+    margin-left: 10px;
     margin-right: -8px;
+    padding-left: 10px;
+    padding-top: 16px;
+    padding-bottom: 16px;
+    
   }
   i{
     
-    align-items: flex-start;
   }
 </style>
